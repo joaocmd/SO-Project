b99bd9f687a5317d5cd883e6f134c02e84be6ef7
João David <joaodavid1999@hotmail.com>
2018-11-24 00:21:25 +0000

    doesn't work

--- CircuitRouter-AdvShell/CircuitRouter-Client.c
+++ CircuitRouter-AdvShell/CircuitRouter-Client.c
@@ -34,7 +34,7 @@ void displayUsage(const char* appName) {
 void parseArgs(int argc, char** argv) {
     if (argc != 2) {
         displayUsage(argv[0]);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     if (access(argv[1], F_OK) == -1) {
         fprintf(stderr, "Invalid pipe path.\n");
@@ -58,38 +58,42 @@ int main(int argc, char** argv) {
     // Open server pipe
     if ((fserv = open(serverPipe, O_WRONLY)) < 0) {
         fprintf(stderr, "Client: couldn't open AdvShell pipe.\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     char clientPipe[MAXPIPELEN]; 
     int fcli;
-    // Open client pipe
+    // Create client pipe
     snprintf(clientPipe, MAXPIPELEN, "/tmp/advclient%i.pipe", getpid());
     unlink(clientPipe);
     if (mkfifo(clientPipe, 0666) < 0) {
         fprintf(stderr, "Client: couldn't create client pipe.\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
-    if ((fcli = open(clientPipe, O_RDONLY | O_NONBLOCK)) < 0) {
+    if ((fcli = open(clientPipe, O_RDONLY | O_NONBLOCK) < 0)) {
         fprintf(stderr, "Client: couldn't open client pipe.\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     char buffer[BUFFERSIZE];
+    bool_t ready = FALSE;
     while (1) {
         char* line = fgets(buffer, BUFFERSIZE, stdin);
         if (line == NULL) {
+            fprintf(stderr, "%s\n", buffer);
             fprintf(stderr, "Error reading input, terminating.\n");
-            exit(1);
+            exit(EXIT_FAILURE);
         }
        
         // Send any command to the advanced shell.
         char msg[BUFFERSIZE];
         snprintf(msg, BUFFERSIZE, "%s%c%s", clientPipe, CLIMSGDELIM, buffer);
         write(fserv, msg, strlen(msg) + 1);
-        read(fcli, msg, BUFFERSIZE);
-        printf("%s\n", msg);
+
+        int bread = read(fcli, msg, BUFFERSIZE);
+        msg[bread] = '\0';
+        printf("%i\n%s", bread, msg);
     }
 
-    exit(0);
+    exit(EXIT_SUCCESS);
 }
